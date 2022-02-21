416b761038997b8887828a9edf5219488dd7443e
Ravi vardhan <ravivardhanbtechcse@gmail.com>
2022-02-22 00:11:45 +0530

    series

--- Web Technologies/inverse.php
+++ Web Technologies/inverse.php
@@ -5,12 +5,16 @@
 
     $x=2;
     $n=3;
+    $series_str="";
     while($num<$n+1)
     {
         $series=$series+(1/pow($x,$num));
+        $div=pow($x,$num);
+        $series_str=$series_str."1/$div,";
         $num=$num+1;
 
     }
+    echo "<br>Series :".$series_str;
     echo "<br>The value of the series is: ".$series;
 
 
--- Web Technologies/xdivide.php
+++ Web Technologies/xdivide.php
@@ -3,14 +3,19 @@
 
     $num=1;
 
-    $x=5;
-    while($num<3)
+    $x=1;
+    $series_str="";
+    while($num<=10)
     {
         $series=$series+(1/($num*$x));
+        $div=$num*$x;
+        $series_str=$series_str."1/$div,";
         $num=$num+1;
 
     }
-    echo $series;
+    //echo $series;
+    echo "<br>Series :".$series_str;
+    echo "<br>Series value :".round($series,3);
 
 
 
